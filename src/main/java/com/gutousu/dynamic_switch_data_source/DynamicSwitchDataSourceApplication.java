4c13b09a05d47c9e6bab1e991c29818fb81894b4
gutousu <785988673@qq.com>
2018-12-03 10:57:32 +0800

    test

--- src/main/java/com/gutousu/dynamic_switch_data_source/DynamicSwitchDataSourceApplication.java
+++ src/main/java/com/gutousu/dynamic_switch_data_source/DynamicSwitchDataSourceApplication.java
@@ -2,7 +2,7 @@ package com.gutousu.dynamic_switch_data_source;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class DynamicSwitchDataSourceApplication {
 
